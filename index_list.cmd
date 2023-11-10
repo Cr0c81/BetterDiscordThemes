f6aa4c78af536ca30009982885db2b5b951f816b
Иван Малицкий <sms0.mail.ru@gmail.com>
2023-11-10 11:59:27 +0300

    wip index

--- index_list.cmd
+++ index_list.cmd
@@ -1,4 +1,4 @@
 type index0.txt>index.html
 dir *.jpg /b > filelist.txt
-for /F "eol=; tokens=*" %%i in (filelist.txt) do echo "<img src=%%i><h4>%%i</h4>">>index.html
+for /F "eol=; tokens=*" %%i in (filelist.txt) do echo ^<img src=%%i width=256px onclick=CopyToClipboard("%%i")^>^<br^>%%i^<br^> >>index.html
 type index1.txt>>index.html
