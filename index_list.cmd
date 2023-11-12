4a865bbcf8fae4ad48b471007ba0a4ea21eaa7a7
Иван Малицкий <sms0.mail.ru@gmail.com>
2023-11-12 10:24:31 +0300

    wip

--- index_list.cmd
+++ index_list.cmd
@@ -1,5 +1,6 @@
 type index0.txt>index.html
 dir *.jpg *.png *.jpeg /b > filelist.txt
-for /F "eol=; tokens=*" %%i in (filelist.txt) do echo ^<img src=%%i width=256px onclick=CopyToClipboard("%%i")^>^<br^>%%i^<br^> >>index.html
+//rem for /F "eol=; tokens=*" %%i in (filelist.txt) do echo ^<img src=%%i width=256px onclick=CopyToClipboard("%%i")^>^<br^>%%i^<br^> >>index.html
+for /F "eol=; tokens=*" %%i in (filelist.txt) do echo ^<table style="display:inline;"^>^<tr^>^<th^>^<img src=%%i width=256px onclick=CopyToClipboard("%%i")^>^</th^>^</tr^>^<tr^>^<th^>%%i^</th^>^</tr^>^</tr^>^</table^> >>index.html
 type index1.txt>>index.html
 del filelist.txt
