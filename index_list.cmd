b0077d49e9f0ef361fe97a4f952a2c401ec9e73e
Иван Малицкий <sms0.mail.ru@gmail.com>
2024-05-19 15:56:26 +0300

    fix slider, add foldable section

--- index_list.cmd
+++ index_list.cmd
@@ -1,6 +1,24 @@
+echo off
 type index0.html>index.html
-dir *.jpg *.png *.jpeg /b > filelist.txt
-//rem for /F "eol=; tokens=*" %%i in (filelist.txt) do echo ^<img src=%%i width=256px onclick=CopyToClipboard("%%i")^>^<br^>%%i^<br^> >>index.html
-for /F "eol=; tokens=*" %%i in (filelist.txt) do echo ^<table style="display:inline-grid;" class="imgtd"^>^<tr^>^<th^>^<img src=%%i width=256px onclick=CopyToClipboard("%%i") class="imgel"^>^</th^>^</tr^>^<tr^>^<th^>%%i^</th^>^</tr^>^</tr^>^</table^> >>index.html
+dir /A:D /B > dirs.txt
+for /F "eol=; tokens=*" %%i in (dirs.txt) do if NOT "%%i"==".idea" if NOT "%%i"=="fonts" if NOT "%%i"==".git" CALL :create_filelist %%i
+
+rem CALL :create_filelist common
+rem CALL :create_filelist girls
+rem CALL :create_filelist girls_mobile
 type index1.html>>index.html
+goto exit
+
+:create_filelist
+SETLOCAL
+set DIR=%1
+cd %DIR%
+dir *.jpg *.png *.jpeg /b > filelist.txt
+echo ^<details^>^<summary^>%DIR%^</summary^> >>..\index.html
+for /F "eol=; tokens=*" %%i in (filelist.txt) do echo ^<table style="display:inline-grid;" class="imgtd"^>^<tr^>^<th^>^<img src=%DIR%/%%i width=256px onclick=CopyToClipboard("%DIR%/%%i") class="imgel"^>^</th^>^</tr^>^<tr^>^<th^>%%i^</th^>^</tr^>^</tr^>^</table^> >>..\index.html
+echo ^</details^> >>..\index.html
 del filelist.txt
+cd..
+ENDLOCAL
+
+:exit
