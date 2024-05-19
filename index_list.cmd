d4c944cafc0436301186ef983b601ff1be057b11
Иван Малицкий <sms0.mail.ru@gmail.com>
2024-05-19 14:00:19 +0300

    add BB colored text function

--- index_list.cmd
+++ index_list.cmd
@@ -1,7 +1,6 @@
-type index0.txt>index.html
+type index0.html>index.html
 dir *.jpg *.png *.jpeg /b > filelist.txt
 //rem for /F "eol=; tokens=*" %%i in (filelist.txt) do echo ^<img src=%%i width=256px onclick=CopyToClipboard("%%i")^>^<br^>%%i^<br^> >>index.html
 for /F "eol=; tokens=*" %%i in (filelist.txt) do echo ^<table style="display:inline-grid;" class="imgtd"^>^<tr^>^<th^>^<img src=%%i width=256px onclick=CopyToClipboard("%%i") class="imgel"^>^</th^>^</tr^>^<tr^>^<th^>%%i^</th^>^</tr^>^</tr^>^</table^> >>index.html
-rem for /F "eol=; tokens=*" %%i in (filelist.txt) do echo ^<table style="display:block;" class="imgtd"^>^<tr^>^<img src=%%i width=256px onclick=CopyToClipboard("%%i")^>^</tr^>^<tr^>%%i^</tr^>^</tr^>^</table^> >>index.html
-type index1.txt>>index.html
+type index1.html>>index.html
 del filelist.txt
