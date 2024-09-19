312b6eb36e9e7a33caaaf9ac2fe4c56ac9715e17
Иван Малицкий <sms0.mail.ru@gmail.com>
2024-09-19 23:46:05 +0300

    switch width to horizontal count

--- index_list.cmd
+++ index_list.cmd
@@ -1,9 +1,9 @@
 echo off
-type index0.txt>index.html
+type index0.html>index.html
 dir /A:D /B > dirs.txt
 for /F "eol=; tokens=*" %%i in (dirs.txt) do if NOT "%%i"==".idea" if NOT "%%i"=="fonts" if NOT "%%i"==".git" if NOT "%%i"=="iptv" CALL :create_filelist %%i
 
-type index1.txt>>index.html
+type index1.html>>index.html
 del dirs.txt
 goto exit
 
@@ -13,7 +13,7 @@ set DIR=%1
 cd %DIR%
 dir *.jpg *.png *.jpeg /b /O:N > filelist.txt
 echo ^<details^>^<summary^>%DIR%^</summary^> >>..\index.html
-for /F "eol=; tokens=*" %%i in (filelist.txt) do echo ^<table style="display:inline-grid;" class="imgtd"^>^<tr^>^<th^>^<img src=%DIR%/%%i width=256px onclick=CopyToClipboard("%DIR%/%%i") class="imgel"^>^</th^>^</tr^>^<tr^>^<th^>%%i^</th^>^</tr^>^</tr^>^</table^> >>..\index.html
+for /F "eol=; tokens=*" %%i in (filelist.txt) do echo ^<table style="display:inline-grid;" class="imgtd"^>^<tr^>^<th^>^<img src=%DIR%/%%i width=256px onclick=CopyToClipboard("%DIR%/%%i") class="imgel"^>^</th^>^</tr^>^<tr^>^<th class="textdesc"^>%%i^</th^>^</tr^>^</tr^>^</table^> >>..\index.html
 echo ^</details^> >>..\index.html
 del filelist.txt
 cd..
