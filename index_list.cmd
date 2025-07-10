c752d058c9b5e8cfb3fb075d3c0ac64b0313255a
KpoKec <sms0.mail.ru@gmail.com>
2025-07-10 14:07:54 +0300

    wip

--- index_list.cmd
+++ index_list.cmd
@@ -5,7 +5,7 @@ del dirs.html
 for /F "eol=; tokens=*" %%i in (dirs_temp.txt) do if NOT "%%i"==".idea" if NOT "%%i"=="fonts" if NOT "%%i"==".git" if NOT "%%i"=="iptv" IF NOT "%%i"=="_data" CALL :create_filelist %%i
 
 del dirs_temp.txt
-imglist.exe
+imglist.exe > images.html
 del dirs.html
 goto exit
 
@@ -15,7 +15,11 @@ echo %1>>dirs.html
 SETLOCAL
 set DIR=%1
 cd %DIR%
-dir *.jpg *.png *.jpeg /b /O:N>..\%1.html
+CALL :convert2jpg %1 %%i
+
+rename *.jpeg *.jpg > nul
+rename *.jfif *.jpg > nul
+dir *.jpg /b /O:N>..\%1.html
 if exist preview del /F /Q preview\*.*
 if NOT exist preview mkdir preview
 for /F "eol=; tokens=*" %%i in (..\%1.html) do CALL :convert %1 %%i
@@ -24,7 +28,15 @@ cd..
 ENDLOCAL
 
 :exit
+GOTO:eof
 
 :convert
+@SET var=%cd%
+@if NOT ["%2"]==[""] "%iview%" %var%\%2 /silent /resize_long=512 /jpgq=90 /resample /aspectratio /convert="%var%\preview\%2"
+GOTO:eof
+
+:convert2jpg
 SET var=%cd%
-if NOT ["%2"]==[""] "%iview%" %var%\%2 /silent /resize_long=512 /resample /aspectratio /convert="%var%\preview\%2"
+"%iview%" %var%\*.png /silent /jpgq=90 /convert="%var%\*.jpg"
+del %var%\*.png
+GOTO:eof
